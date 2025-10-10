4bc2426bcfb1850e3a3ae18d30f379fb9dc59445
DiegoSZ675 <diego.asanchezz08@gmail.com>
2025-10-10 03:32:53 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,15 +1,17 @@
 <?php
-// Credenciales obtenidas de tu proyecto en Supabase
-$host     = "db.fnnpvubtyznoenanmcud.supabase.co";
-$port     = "5432";
-$dbname   = "postgres";
-$user     = "postgres";
-$password = "4J3V6hcYgGe2Pc";
+// Leer las credenciales desde las Variables de Entorno de Render
+$host     = getenv('DB_HOST');
+$port     = getenv('DB_PORT'); // <-- Leemos el nuevo puerto
+$dbname   = getenv('DB_NAME');
+$user     = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+
+// --- No modifiques el código de abajo ---
 
 // Crear la cadena de conexión
 $conn_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password}";
 
-// Establecer la conexión con la función para PostgreSQL
+// Establecer la conexión
 $conn = pg_connect($conn_string);
 
 // Verificar si la conexión falló
